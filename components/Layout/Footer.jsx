c15b45050bf803a4aecab77ac194de52330892ab
riandyhasan <riandyhsn@gmail.com>
2023-10-28 16:59:21 +0700

    feat: wop footer

---
+++ components/Layout/Footer.jsx
@@ -0,0 +1,7 @@
+import Image from 'next/image';
+import styles from '../../styles/Layout.module.css';
+import Link from 'next/link';
+
+export default function Footer() {
+    return <footer className={styles.footer}>Inklusify</footer>;
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,4 @@
+import Footer from '../components/Layout/Footer';
 import Header from '../components/Layout/Header';
 import '../styles/globals.css';
 
@@ -6,6 +7,7 @@ function MyApp({ Component, pageProps }) {
         <div>
             <Header />
             <Component {...pageProps} />
+            <Footer />
         </div>
     );
 }
